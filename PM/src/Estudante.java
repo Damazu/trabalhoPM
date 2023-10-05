6937f2872262ef4d738d75fbef322e306c27a547
yvinilord <yvinilordsalles@gmail.com>
2023-10-05 10:27:40 -0300

    hehe

--- PM/src/Estudante.java
+++ PM/src/Estudante.java
@@ -1,19 +1,36 @@
 import java.sql.Time;
 import java.util.Date;
+import java.util.Scanner;
 
-public class Estudante {
+public class Estudante extends Pessoa{
     private float nota;
     private int matricula;
     private Minicurso curso;
 
-      public Estudante(float nota, int matricula, Minicurso curso) {
+    public Estudante(float nota, int matricula, Minicurso curso) {
+        super(nome,cpf);
+        this.nome = nome;
+        this.cpf = cpf;
         this.nota = nota;
         this.matricula = matricula;
         this.curso = curso;
     }
 
-     // Métodos getters e setters (para acessar e modificar os campos)
-     public Float getnota() {
+    Scanner sc = new Scanner(System.in);
+
+    public Estudante matricularEstudante(){
+        System.out.println("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("CPF: ");
+        String cpf = sc.nextLine();
+
+        return new Estudante(nome, cpf, nota, matricula, curso);
+    }
+
+    //Métodos getters e setters (para acessar e modificar os campos)
+
+    public Float getnota() {
         return nota;
     }
 
@@ -37,12 +54,5 @@ public class Estudante {
         this.curso = curso;
     }
 
-    public Estudante matricularEstudante(){
-
-        return new Estudante(nota, matricula, curso);
-    }
-    public void realizarTrabalho(){}
-
-    public void apresentarTrabalho(){}
 
 }

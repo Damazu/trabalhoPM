4829e4709df21d71b862cf16fc508dd92a851b69
ViniiSalles <yvinilordsalles@gmail.com>
2023-10-06 16:07:57 -0300

    .

--- PM/src/App.java
+++ PM/src/App.java
@@ -4,18 +4,29 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um valor: ");
         int value = sc.nextInt();
 
-       switch (value) {
-        case 1:
-                Estudante estudante = Estudante.matricularEstudante(0, sc);
-                System.out.println("Nome: " + estudante.getNome());
-            break;
-       
-        default:
-            sc.close();
-            break;
-       }
+        switch (value) {
+            case 1:
+                Estudante estudante = new Estudante("", "", 0, 0);
+
+                estudante.matricularEstudante();
+                estudante.exibirInformacoes();
+                break;
+
+            case 2:
+                Professor professor = new Professor("", "", "", 0);
+
+                professor.cadastroProfessor();
+                professor.exibirInformacoes();
+                break;
+                
+            default:
+                sc.close();
+                break;
+        }
 
     }
 }

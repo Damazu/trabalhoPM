31b65e4f432306b501f5682d0696701094c4028c
ViniiSalles <yvinilordsalles@gmail.com>
2023-10-06 21:30:05 -0300

    whileTrue

--- PM/src/App.java
+++ PM/src/App.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
@@ -5,37 +6,63 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite um valor: ");
-        int value = sc.nextInt();
+        while (true) {
+            System.out.println("Digite um valor: ");
+            int value = sc.nextInt();
 
-        switch (value) {
-            case 1:
-                Estudante estudante = Estudante.getestudante(); // Obter a instância única
+            switch (value) {
+                case 1:
+                    Estudante estudante = Estudante.getestudante(); // Obter a instância única
 
-                estudante.matricularEstudante();
-                estudante.exibirInformacoes();
-                break;
+                    estudante.matricularEstudante();
+                    estudante.exibirInformacoes();
+                    break;
 
-            case 2:
-                Professor professor = Professor.cadastroprofessor();
+                case 2:
+                    Professor professor = Professor.cadastroprofessor();
 
-                professor.exibirInformacoes();
-                break;
+                    professor.exibirInformacoes();
+                    break;
 
-            case 3:
-                Apresentação apresentação = new Apresentação(null);
-                
-                apresentação.agendarApresentação();
-                break;
+                case 3:
+                    Apresentação apresentação = new Apresentação(null);
 
-            case 9:
-                Universidade universidade = Universidade.cadastropUniversidade();
-                System.out.println("Nome da universidade: " + universidade.getNome());
-                break;
+                    apresentação.agendarApresentação();
+                    break;
 
-            default:
-                sc.close();
-                break;
+                case 9:
+                    Universidade universidade = Universidade.cadastropUniversidade();
+                    System.out.println("Nome da universidade: " + universidade.getNome());
+                    break;
+
+                case 0:
+                    System.out.println("Programa Finalizado.");
+                    System.exit(0); // Encerre o programa
+
+                default:
+                    sc.close();
+                    break;
+            }
+
+            System.out.println("\nPressione Enter para voltar ao menu...");
+               sc.nextLine();  // Limpar o buffer do scanner
+               sc.nextLine();  // Aguardar pressionar Enter para continuar
+
+               // Limpar a tela
+               clearScreen();
+        }
+    }
+
+    private static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException e) {
+            // Tratar exceções, se necessário
+            e.printStackTrace();
         }
     }
 }
